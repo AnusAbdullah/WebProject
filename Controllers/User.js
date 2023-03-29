bb557f183bd67cb947540e40aebd000d892b188e
arbazameerk <arbazameer.k@gmail.com>
2023-03-29 06:22:34 +0500

    User Authentication and Management.

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,21 +1,27 @@
+// Require the necessary modules and models
 const user = require("../Model/User.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// Function to add a new user to the database
 const addUser = async (req, res, next) => {
+   // Get the user data from the request body
   const { fname, lname, email, password } = req.body;
 
+  // Hash the password using bcrypt
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
   // console.log(cname, cemail, cpassword, caddress);
   try {
     user.create({
+      // Create a new user in the database with the hashed password
       fname: fname,
       lname: lname,
       email: email,
       password: hashedPassword,
     });
+    // Send a response with the user data
     res.send({
       fname: fname,
       lname: lname,
@@ -23,58 +29,69 @@ const addUser = async (req, res, next) => {
       password: hashedPassword,
     });
   } catch (err) {
+     // If there's an error, send a response with an error message
     res.status(500).send({
       message: err.message || "Some error occurred while creating the User.",
     });
   }
 };
 
+// Function to sign in a user and generate a JWT token
 const SignIn = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Check if user exists
+    // Check if the user exists in the database
     const User = await user.findOne({ email });
     if (!User) {
+       // If the user doesn't exist, send an error response
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
     // Check if password is correct
     const isMatch = await bcrypt.compare(password, User.password);
     if (!isMatch) {
+      // If the password is incorrect, send an error response
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Generate JWT token
+    // If the user exists and the password is correct, generate a JWT token
     const token = jwt.sign({ id: User._id }, "mysecretkey", {
       expiresIn: "300s",
     });
-
+    // Send the JWT token in the response
     res.json({ token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
-
+// Middleware function to validate a JWT token
 const validateToken = (req, res, next) => {
+  // Get the authorization header from the request
   const authHeader = req.headers.authorization;
   if (!authHeader) {
+    // If there's no authorization header, send an error response
     return res.status(401).json({ message: "Unauthorized" });
   }
 
+  // Get the JWT token from the authorization header
   const token = authHeader.split(" ")[1];
 
   try {
+    // Verify the JWT token and get the user ID from the payload
     const decoded = jwt.verify(token, "mysecretkey");
     req.UserId = decoded.id;
+    // Call the next middleware function
     next();
   } catch (error) {
+     // If the token is invalid, send an error response
     console.error(error);
     res.status(401).json({ message: "Invalid token" });
   }
 };
 
+// Function to send a response indicating the user is authenticated
 const Profile = (req, res) => {
   res.json({ message: "You are authenticated!" });
 };
